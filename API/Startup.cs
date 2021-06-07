0f5632b5eb47e405d7f2ad8cd85a503fa822134d
Realdo Dias <realdo.dias3@gmail.com>
2021-06-07 17:25:05 +0800

    add sqlite and create data context

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,48 +12,54 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+          public Startup(IConfiguration configuration)
+          {
+               Configuration = configuration;
+          }
 
-        public IConfiguration Configuration { get; }
+          public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+          // This method gets called by the runtime. Use this method to add services to the container.
+          public void ConfigureServices(IServiceCollection services)
+          {
+               services.AddDbContext<DataContext>(options => 
+               {
+                    options.UseSqlite("Connection string");
+               });
+               services.AddControllers();
+               services.AddSwaggerGen(c =>
+               {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+               });
+          }
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-        }
+          // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+          public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+          {
+               if (env.IsDevelopment())
+               {
+                    app.UseDeveloperExceptionPage();
+                    app.UseSwagger();
+                    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+               }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
+               app.UseHttpsRedirection();
 
-            app.UseHttpsRedirection();
+               app.UseRouting();
 
-            app.UseRouting();
+               app.UseAuthorization();
 
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+               app.UseEndpoints(endpoints =>
+               {
+                    endpoints.MapControllers();
+               });
+          }
     }
 }
