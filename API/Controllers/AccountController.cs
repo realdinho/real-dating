ea6dbcccb9afb2882bb4099b9ad840b1a9988707
Realdo Dias <realdo.dias3@gmail.com>
2021-06-13 22:57:40 +0800

    add registration DTO and update registration endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,8 +2,10 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -16,21 +18,28 @@ namespace API.Controllers
           }
 
           [HttpPost("register")]
-          public async Task<ActionResult<AppUser>> Register(string username, string password) 
+          public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO) 
           {
+               if (await UserExists(registerDTO.Username.ToLower())) return BadRequest("Username is taken");
+
                using var hmac = new HMACSHA512();
 
                var user = new AppUser
                {
-                    UserName = username,
-                    PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                    UserName = registerDTO.Username,
+                    PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
                     PasswordSalt = hmac.Key
                };
 
                _context.Users.Add(user);
-               await _context.SaveChangesAsync();
+               await _context.SaveChangesAsync(); 
 
                return user;
+          }
+
+          private async Task<bool> UserExists(string username)
+          {
+               return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
           }
      }
 }
