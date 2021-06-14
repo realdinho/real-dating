7d906b259cfcd04157c3ef43d637379b7c4cf842
Realdo Dias <realdo.dias3@gmail.com>
2021-06-14 13:00:00 +0800

    add extension methods

--- API/Startup.cs
+++ API/Startup.cs
@@ -19,6 +19,7 @@ using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using API.Extensions;
 
 namespace API
 {
@@ -33,28 +34,14 @@ namespace API
           // This method gets called by the runtime. Use this method to add services to the container.
           public void ConfigureServices(IServiceCollection services)
           {
-               services.AddScoped<ITokenService, TokenService>();
-               services.AddDbContext<DataContext>(options =>
-               {
-                    options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-               });
+               services.AddApplicationServices(_config);
                services.AddControllers();
                services.AddCors();
                services.AddSwaggerGen(c =>
                {
                     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
                });
-               services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                    .AddJwtBearer(options => 
-                    {
-                         options.TokenValidationParameters = new TokenValidationParameters
-                         {
-                              ValidateIssuerSigningKey = true,
-                              IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
-                              ValidateIssuer = false,
-                              ValidateAudience  = false,
-                         };
-                    });
+               services.AddIdentityServices(_config);
           }
 
           // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,7 +61,7 @@ namespace API
                app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
                app.UseAuthentication();
-               
+
                app.UseAuthorization();
 
                app.UseEndpoints(endpoints =>
